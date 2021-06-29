9cb0f91941f0d6870ed297759dde874b265b2ed6
Jilvo <55281440+Jilvo@users.noreply.github.com>
2021-06-29 21:27:51 +0200

    accordion home page, DB home page

--- actus/models.py
+++ actus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Savoir(models.Model):
+    """Savoir class"""
+
+    title = models.CharField(max_length=150)
+    date = models.CharField(max_length=50)
+    link_yt = models.CharField(max_length=200)
+    link_pdf = models.CharField(max_length=100)
+    ouvert_par_defaut = models.CharField(max_length=10, default="",blank=True)
+    disponible = models.BooleanField()
+
+    def __str__(self):
+        return self.title
--- actus/views.py
+++ actus/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
+
+from .models import Savoir
+
 # Create your views here.
 
 def home_function(request):
     """Render the Savoir page"""
-    return render(request,"index.html")
+    actus_list = Savoir.objects.all().order_by('-date')
+    list_dispo = []
+    list_pas_dispo = []
+    print(actus_list)
+    for actu_no in actus_list:
+        if actu_no.disponible == True:
+            list_dispo.append(actu_no)
+            # dict_actus["dispo"] = actu_no
+        else:
+            list_pas_dispo.append(actu_no)
+            # dict_actus["pas_dispo"] = actu_no
+    context = {"actus_dispo" : list_dispo,"actus_pas_dispo" : list_pas_dispo} 
+    return render(request,"index.html",context)
 
 def connaitre_function(request):
     """Render the Connaitre page"""
