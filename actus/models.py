e314bd1f633ba7960e1d2a6bd338a7d1ea325ed1
Jilvo <55281440+Jilvo@users.noreply.github.com>
2021-06-30 22:58:00 +0200

    suivre page, few change home page

--- actus/models.py
+++ actus/models.py
@@ -7,12 +7,41 @@ class Savoir(models.Model):
 
     title = models.CharField(max_length=150)
     date = models.CharField(max_length=50)
-    link_yt = models.CharField(max_length=200)
-    link_pdf = models.CharField(max_length=100)
+    link_yt = models.CharField(max_length=200,blank=True)
+    link_pdf = models.CharField(max_length=100,blank=True)
     ouvert_par_defaut = models.CharField(max_length=10, default="",blank=True)
     disponible = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+         ordering = ['created_at']
+         
+    def __str__(self):
+        return self.title
+
+class Actusuivre(models.Model):
+    """Actusuivre class"""
+
+    title = models.CharField(max_length=150)
+    link = models.CharField(max_length=200,blank=True)
+    date = models.CharField(max_length=50,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+         ordering = ['created_at']
+         
+    def __str__(self):
+        return self.title
+
+class Agendasuivre(models.Model):
+    """Actusuivre class"""
+
+    title = models.CharField(max_length=150)
+    lieu = models.CharField(max_length=50,blank=True)
+    date = models.CharField(max_length=50,blank=True)
+    message = models.CharField(max_length=200,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
          ordering = ['created_at']
          
